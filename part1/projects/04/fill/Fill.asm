1ee265cfc5ff4536e2f1a76c573bb3ce5242c949
Ephraim Kunz <ephraimkunz@me.com>
2019-12-18 07:32:44 -0800

    Project 4 complete

--- part1/projects/04/fill/Fill.asm
+++ part1/projects/04/fill/Fill.asm
@@ -11,4 +11,71 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LOOP)
+    @KBD
+    D=M
+
+    @KEY_PRESSED
+    D;JGT
+
+(KEY_NOT_PRESSED)
+    // Init wordCounter to 8191
+    @8191 // 32 words per row * 256 rows - 1 (zero based indexing)
+    D=A
+    @wordCounter
+    M=D
+
+(KEY_NOT_PRESSED_FILL_SCREEN_LOOP)
+    @LOOP_END
+    D;JLT
+
+    // Calculate position and fill
+    @SCREEN
+    D=D+A
+    A=D
+    M=0
+
+    // Decrement wordCounter
+    @wordCounter
+    D=M
+    M=D-1
+
+    // Loop again
+    @wordCounter
+    D=M 
+
+    @KEY_NOT_PRESSED_FILL_SCREEN_LOOP
+    0;JMP
+
+(KEY_PRESSED)
+// Init wordCounter to 8191
+    @8191 // 32 words per row * 256 rows - 1 (zero based indexing)
+    D=A
+    @wordCounter
+    M=D
+
+(KEY_PRESSED_FILL_SCREEN_LOOP)
+    @LOOP_END
+    D;JLT
+
+    // Calculate position and fill
+    @SCREEN
+    D=D+A
+    A=D
+    M=-1
+
+    // Decrement wordCounter
+    @wordCounter
+    D=M
+    M=D-1
+
+    // Loop again
+    @wordCounter
+    D=M 
+
+    @KEY_PRESSED_FILL_SCREEN_LOOP
+    0;JMP
+
+(LOOP_END)
+    @LOOP
+    0;JMP
